114dc006d3d824c7df914be59fe11e2ec9dbb50d
James Woolfenden <James.Woolfenden@gmail.com>
2021-06-13 16:42:20 +0100

    refresh

--- aws_acm_certificate_validation.check.tf
+++ aws_acm_certificate_validation.check.tf
@@ -1,4 +1,4 @@
 resource "aws_acm_certificate_validation" "check" {
   certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [aws_route53_record.valid.0.fqdn]
+  validation_record_fqdns = ["${var.sub_domain}.${var.domain}"]
 }
